6ef1619094f59d199b7360107cfd1efd8cc1cf81
Wenxuan Su <35317989+stony-su@users.noreply.github.com>
2024-07-27 15:35:00 -0400

    added dense and activation block

--- blocky/blocks.py
+++ blocky/blocks.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 class Block:
     def __init__(self, name, parameters):
         self.name = name
@@ -6,3 +7,32 @@ class Block:
     def to_code(self):
         # Convert block to TensorFlow code
         pass
+
+class DenseBlock:
+    def __init__(self, units, activation=None):
+        self.units = units
+        self.activation = activation
+        self.layer = tf.keras.layers.Dense(units, activation=activation)
+
+    def apply(self, inputs):
+        return self.layer(inputs)
+
+    def get_config(self):
+        return {
+            'units': self.units,
+            'activation': self.activation
+        }
+
+
+class ActivationBlock:
+    def __init__(self, activation):
+        self.activation = activation
+        self.layer = tf.keras.layers.Activation(activation)
+
+    def apply(self, inputs):
+        return self.layer(inputs)
+
+    def get_config(self):
+        return {
+            'activation': self.activation
+        }
