03d2ff840836e08edd3d9e3d9d8159c5076be00a
eddieb <eddiewird@gmail.com>
2024-07-27 15:27:45 -0400

    setup change stuff

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package',
+    name='blocky',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'tensorflow>=2.0',
         # other dependencies
     ],
-    author='Your Name',
+    author='Darren, Eddie, Kahan, Richard',
     description='A block-based framework for TensorFlow',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/your_package',
+    url='https://github.com/stony-su/starterhacks',
 )
