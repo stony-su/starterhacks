3eb70dbc3474fcc1f15c01e52c24423859d4fd6e
eddieb <eddiewird@gmail.com>
2024-07-27 17:24:24 -0400

    chore: Update package version to 1.0.3

--- build/lib/tensor_builder/__init__.py
+++
@@ -1,7 +0,0 @@
-# your_package/__init__.py
-
-from .blocks.blocks import Block
-from .codegen import generate_code
-from .backend import execute_code
-
-__all__ = ['Block', 'generate_code', 'execute_code']
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tensor_build',
-    version='0.3',
+    version='1.0.3',
     packages=find_packages(),
     install_requires=[
         'tensorflow>=2.0',
