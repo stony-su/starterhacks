0c2b5c75d0c0b95735335059c664d45cb044e7e8
eddieb <eddiewird@gmail.com>
2024-07-27 20:02:02 -0400

    updated the file pypi thing and also changed the setup

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -1,5 +1,7 @@
 import unittest
 from tensor_build.blocks.conv_2D_block import Conv2DBlock
+import tensor_build
+from tensor_build import compile
 
 class TestBlock(unittest.TestCase):
     def test_block_initialization(self):
@@ -7,6 +9,7 @@ class TestBlock(unittest.TestCase):
         self.assertEqual(block.filters, 32)
         self.assertEqual(block.kernel_size, (3, 3))
         self.assertEqual(block.activation, 'relu')
+        self.compile
 
 if __name__ == '__main__':
     unittest.main()
